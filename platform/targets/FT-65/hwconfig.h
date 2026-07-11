ede9761e7a87ae138a90c717d8d8e748cd1ecb01
sdrgeek <sdrgeek@protonmail.com>
2026-07-11 14:15:56 +0200

    feat(ft65): add directory "scaffolding"

---
+++ platform/targets/FT-65/hwconfig.h
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2020-2026 OpenRTX Contributors
+ * 
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef HWCONFIG_H
+#define HWCONFIG_H
+
+#include "gd32f10x.h"
+#include "pinmap.h"
+
+#endif 
